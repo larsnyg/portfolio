5a4ccd0f1f4836d08bfdb76df1698c4692517b9d
Lars Nygaard <lars@Larss-MacBook-Air.local>
2022-07-13 22:37:45 +0200

    API test

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 console.log("Hello!")
+
+const contentful = require("contentful");
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: "pdggcpu3a4hl",
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: "pdggcpu3a4hl"
+});
+// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
+client
+  .getEntry("7fZqq2gqWPDyQI0OEcgNgT")
+  .then(entry => console.log(entry))
+  .catch(err => console.log(err));
