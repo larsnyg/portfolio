c4daedab4cf6f2067368c0c0c702dff21c5706e6
Lars Nygaard <lars@Larss-MacBook-Air.local>
2022-07-13 22:43:05 +0200

    Updated access token

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ const client = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
   space: "pdggcpu3a4hl",
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: "pdggcpu3a4hl"
+  accessToken: "8o_8-gueoYKvenSSjNrXJYd4SiLkJO8yhHEik9734uw"
 });
 // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
 client
